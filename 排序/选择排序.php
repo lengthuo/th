90215e20c98c67190a62262511e9f0c4c1c41c87
李硕 <598684053@qq.com>
2017-11-05 15:08:03 +0800

    fix(排序): 选择排序

--- 排序/选择排序.php
+++ 排序/选择排序.php
@@ -9,30 +9,32 @@
  *     而选择排序每遍历一次都记住了当前最小（大）元素的位置，最后仅需一次交换操作即可将其放到合适的位置。
  */
 
-$s = [1, 9, 2, 8, 3, 7, 100, 24, 21];
-$n = count($s);
-
-// 最差时间复杂度 ---- O(n^2)
-// 最优时间复杂度 ---- O(n^2)
-// 平均时间复杂度 ---- O(n^2)
-
-for ($i = 0; $i < $n - 1; ++$i) {
-    $num = $i;
-    for ($j = $i + 1; $j < $n; ++$j) {
-        if ($s[$j] < $s[$num]) {
-            $num = $j;
+function selection_sort($array)
+{
+    $count = count($array);
+    for ($i = 0; $i < $count - 1; $i++) {
+        /*findtheminest*/
+        $min = $i;
+        echo '$min-->' . $array[$min] . '-->';
+        for ($j = $i + 1; $j < $count; $j++) {
+            //由小到大排列
+            if ($array[$min] > $array[$j]) {
+                //表明当前最小的还比当前的元素大
+                $min = $j;
+                //赋值新的最小的
+            }
+        }
+        echo $array[$min] . 'coco<br/>';
+        /*swap$array[$i]and$array[$min]即将当前内循环的最小元素放在$i位置上*/
+        if ($min != $i) {
+            $temp = $array[$min];
+            $array[$min] = $array[$i];
+            $array[$i] = $temp;
         }
     }
-    if ($num != $i) {
-        swap($s, $i, $num);
-    }
+    return $array;
 }
-print_r($s);
 
-
-function swap(&$s, $i, $j)
-{
-    $temp = $s[$i];
-    $s[$i] = $s[$j];
-    $s[$j] = $temp;
-}
+$old_array = [3, 4, 5, 6, 8, 2, 12];
+$new_array = selection_sort($old_array);
+print_r($new_array);
